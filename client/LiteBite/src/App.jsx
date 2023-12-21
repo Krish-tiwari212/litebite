4cafcb90911174b3355cd7808ec4c3501d60b023
siddharth-awasthi <siddharth2004awasthi@gmail.com>
2023-12-22 01:32:38 +0530

    apple

--- client/LiteBite/src/App.jsx
+++ client/LiteBite/src/App.jsx
@@ -1,6 +1,12 @@
 import Onboarding from './views/Onboarding'
+import Login from './views/login'
+import Sign from './views/signup'
 export default function App() {
   return (
+    <>
     <Onboarding/>
+    <Login/>
+    <Sign/>
+    </>
   )
 }
