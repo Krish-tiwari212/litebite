0dab377940211a7a0b31310d6bfbaf3c1b48dbc9
Kadambini30 <kadambinitrivedi@gmail.com>
2023-12-23 03:25:34 +0530

    Navbar

--- client/LiteBite/src/components/navbar.jsx
+++ client/LiteBite/src/components/navbar.jsx
@@ -6,12 +6,12 @@ import home from '../assets/home.svg';
 import search from '../assets/search.svg';
 function navbar() {
 	return (
-		<div className="bg-[#498463CC] w-full h-16 flex flex-row justify-around">
-					<img src={home} alt="" />
-					<img src={heart} alt="" />
-					<img src={search} alt="" />
-					<img src={help} alt="" />
-					<img src={user1} alt="" />
+		<div className="bg-[#498463CC] w-full h-16 flex flex-row justify-around items-center">
+					<img src={home} className='w-10 h-10' />
+					<img src={heart} className='w-10 h-10' />
+					<img src={search} className='w-8 h-8' />
+					<img src={help} className='w-8 h-8' />
+					<img src={user1} className='w-8 h-8' />
 				</div>
 	);
 }
