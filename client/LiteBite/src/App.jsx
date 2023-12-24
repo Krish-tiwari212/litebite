d6cfb5a42c45f5a513f864784848807c4e1a4a9e
siddharth-awasthi <siddharth2004awasthi@gmail.com>
2023-12-24 18:26:16 +0530

    recipe

--- client/LiteBite/src/App.jsx
+++ client/LiteBite/src/App.jsx
@@ -3,10 +3,11 @@ import Sign from './views/signup'
 import Onboarding from './views/Onboarding'
 import Result from './views/Result'
 import Home from './views/Home'
+import Recipe from './views/Recipe'
 export default function App() {
   return (
     <>
-    <Result/>
+    <Recipe/>
     </>
   )
 }
